2cde6bd2b4b6a8388cad0110d0817f925fe535fe
Dina1999 <62246409+Dina1999@users.noreply.github.com>
2020-10-19 20:45:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,161 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('Главная');
+?>
+
+        <!-- /breadcrumbs -->
+        <!-- page -->
+        <div class="page">
+            <!-- content box -->
+            <div class="content-box">
+                <!-- content -->
+                <div class="content">
+                    <div class="cnt">
+                        <header>
+                            <h1>Отзывы</h1>
+                        </header>
+                        <hr>
+                        <div class="review-block">
+                            <div class="review-text">
+
+                                <div class="review-block-title"><span class="review-block-name"><a href="#">Сергей Родионов</a></span><span class="review-block-description">12 мая 2020 г., Генеральный директор, CTC-Медиа</span></div>
+
+                                <div class="review-text-cont">
+                                    Вы сможете организовать внутри компании коллективную работу над проектами в рабочих группах, вести учет и планирование времени и событий, обмениваться сообщениями и почтой через портал, проводить внутри компании видеоконференции и делать многое другое.
+                                </div>
+                            </div>
+                            <div class="review-img-wrap"><a href="#"><img src="img/rew/photo_1.jpg" alt="img"></a></div>
+                        </div>
+                        <div class="review-block">
+                            <div class="review-text">
+                                <div class="review-block-title"><span class="review-block-name"><a href="#">Анна Андреева</a></span><span class="review-block-description">12 мая 2020 г., Маркетолог, RK-Telecom</span></div>
+                                <div class="review-text-cont">
+                                    Вы сможете организовать внутри компании коллективную работу над проектами в рабочих группах, вести учет и планирование времени и событий, обмениваться сообщениями и почтой через портал, проводить внутри компании видеоконференции и делать многое другое.
+                                </div>
+                            </div>
+                            <div class="review-img-wrap"><img src="img/rew/photo_2.jpg" alt="img"></div>
+                        </div>
+                        <div class="review-block">
+                            <div class="review-text">
+                                <div class="review-block-title"><span class="review-block-name"><a href="#">Дмитрия Пялов</a></span><span class="review-block-description">12 мая 2020 г., Программист, .NET-dev</span></div>
+                                <div class="review-text-cont">
+                                    Можете сразу перейти с «младшей», базовой редакции на «Бизнес-процессы». После такого перехода ваш портал можно будет интегрировать с внешним сайтом, на портале добавятся возможности визуального проектирования бизнес-процессов и управления списками, включая списки в рабочих группах. Кроме того, вы сможете анализировать посещаемость своего портала
+                                </div>
+                            </div>
+                            <div class="review-img-wrap"><img src="img/rew/photo_3.jpg" alt="img"></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /content -->
+                <!-- side -->
+                <div class="side">
+                    <!-- side menu -->
+                    <div class="side-block side-menu">
+                        <div class="title-block">Навигация</div>
+                        <div class="menu-block">
+                            <ul>
+                                <li><a href="">Кухни</a>
+                                </li>
+                                <li><a href="">Гарнитуры</a>
+                                </li>
+                                <li class="selected">
+                                    <a href="">Спальни</a>
+                                </li>
+                                <li>
+                                    <a href="">Шкафы</a>
+                                </li>
+                                <li><a href="">Столы</a>
+                                </li>
+                                <li><a href="">Стулья</a>
+                                </li>
+                                <li>
+                                    <a href="">Прихожие</a>
+                                </li>
+                                <li><a href="">Диваны</a>
+                                </li>
+                                <li><a href="">Кресла</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /side menu -->
+                    <!-- side anonse -->
+                    <div class="side-block side-anonse">
+                        <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
+                        <div class="item">
+                            <p>Клиенты предпочитают все больше эко-материалов.</p>
+                        </div>
+                    </div>
+                    <!-- /side anonse -->
+                    <!-- side wrap -->
+                    <div class="side-wrap">
+                        <div class="item-wrap">
+                            <!-- side action -->
+                            <div class="side-block side-action">
+                                <img src="./img/side-action-bg.jpg" alt="" class="bg">
+                                <div class="photo-block">
+                                    <img src="./img/side-action.jpg" alt="">
+                                </div>
+                                <div class="text-block">
+                                    <div class="title">Акция!</div>
+                                    <p>Мебельная полка всего за 560 <span class="r">a</span>
+                                    </p>
+                                </div>
+                                <a href="" class="btn-more">подробнее</a>
+                            </div>
+                            <!-- /side action -->
+                        </div>
+
+                        <!-- footer rew slider box -->
+                        <div class="item-wrap">
+                            <div class="rew-footer-carousel">
+                                <div class="item">
+                                    <div class="side-block side-opin">
+                                        <div class="inner-block">
+                                            <div class="title">
+                                                <div class="photo-block">
+                                                    <img src="./img/side-opin.jpg" alt="">
+                                                </div>
+                                                <div class="name-block"><a href="">Дмитрий Иванов</a></div>
+                                                <div class="pos-block">Генеральный директор,"Офис+"</div>
+                                            </div>
+                                            <div class="text-block">“В магзине предоставили потрясающий выбор
+                                                расцветок, а также, получил большую скидку по карте постоянного...</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <div class="side-block side-opin">
+                                        <div class="inner-block">
+                                            <div class="title">
+                                                <div class="photo-block">
+                                                    <img src="./img/side-opin.jpg" alt="">
+                                                </div>
+                                                <div class="name-block"><a href="">Дмитрий Иванов</a></div>
+                                                <div class="pos-block">Генеральный директор,"Офис+"</div>
+                                            </div>
+                                            <div class="text-block">“В магазине предоставили потрясающий выбор
+                                                расцветок, а также, получил большую скидку по карте постоянного...</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- / footer rew slider box -->
+
+                    </div>
+                    <!-- /side wrap -->
+                </div>
+                <!-- /side -->
+            </div>
+            <!-- /content box -->
+        </div>
+        <!-- /page -->
+        <div class="empty"></div>
+    </div>
+    <!-- /wrap -->
+    <!-- footer -->
+
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
